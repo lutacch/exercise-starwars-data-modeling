859fe3c7aa0a7c89c0575bad63e4db98689a72cf
lutacch <lu.tacchetti@gmail.com>
2022-11-19 10:14:22 +0000

    branch instagram

--- src/models.py
+++ src/models.py
@@ -10,76 +10,56 @@ Base = declarative_base()
 
 class Person(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer , primary_key=True)
     name = Column(String(250), nullable=False)
     user_name = Column(String(250), nullable=False)
+    first_name = Column(String(100))
+    last_name = Column(String(100))
     email = Column(String(50), nullable=False)
     def to_dict(self):
         return {}
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer , primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    user_from_id = Column(Integer, nullable=False)
+    user_to_id = Column(Integer, nullable=False)
+
+    def to_dict(self):
+        return {}
    
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer , primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    
 
-class Character(Base):
-    __tablename__ = 'character'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    height = Column(Integer)
-    hair_color = Column(String(10), nullable=False)
-    skin_color = Column(String(10))
-    eye_color = Column(String(50))
-    birth_year = Column(Integer)
-    gender = Column(String(10))
+    def to_dict(self):
+        return {}
+   
+   
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer , primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    text_inside = Column(String(250))
 
+    def to_dict(self):
+        return {}
+   
+   
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer , primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    url = Column(String(250))
+    post_id = Column(Integer)
 
     def to_dict(self):
         return {}
 
 
-class Planets(Base):
-    __tablename__ = 'planets'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    rotation_period = Column(Integer)
-    orbital_period = Column(Integer)
-    diameter = Column(Integer)
-    climate = Column(String(20))
-    gravity = Column(String(30))
-    terrain = Column(String(30))
-    surface_water = Column(Integer)
-    population = Column(Integer)
-    def to_dict(self):
-        return {}
-
-class Species(Base):
-    __tablename__ = 'species'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    classification = Column(String(50))
-    designation = Column(String(50))
-    average_height = Column(Integer)
-    skin_colors = Column(String(200))
-    hair_colors = Column(String(200))
-    average_lifespan = Column(Integer)
-    language = Column(Integer)
-    def to_dict(self):
-        return {}
-
-class Favorite(Base):
-    __tablename__ = 'favorite'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    character_id = Column(String(250), ForeignKey('character.id'))
-    planet_id = Column((String(250)), ForeignKey('planet.id'))
-
-    def to_dict(self):
-        return {}
 
 
 ## Draw from SQLAlchemy base
